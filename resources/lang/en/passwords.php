934c614d69415ecd223e681e4772e8bdc50d5b66
IU-dev <24-test@bk.ru>
2022-12-24 22:06:46 +0400

    Initial commit

---
+++ resources/lang/en/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ваш пароль сброшен!',
+    'sent' => 'Мы направили ссылку на восстановление пароля в ваш личный кабинет!',
+    'throttled' => 'Подождите немного перед тем, как попробовать еще раз.',
+    'token' => 'Неверный токен безопасности.',
+    'user' => "В базе нет пользователя с таким e-mail.",
+
+];
